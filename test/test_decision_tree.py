726069cdc0f34da4fcca474e4abacd1a9a572e64
Anthony Song <songzixing98@gmail.com>
2020-03-21 21:04:23 +0800

    update unit test

--- test/test_decision_tree.py
+++ test/test_decision_tree.py
@@ -5,31 +5,31 @@ from random_forests.tree_node import TreeNode
 from random_forests.decision_tree import DecisionTree
 
 class TestDecisionTree(unittest.TestCase):
-    @unittest.skip("skip test_get_all_possible_values_on_attribute")
+    # @unittest.skip("skip test_get_all_possible_values_on_attribute")
     def test_get_all_possible_values_on_attribute(self):
         test_dataset = Dataset(_dataset_name = 'watermelon_2.0', _dataset_file_path = './datasets/watermelon.csv')
         test_dataset.load_dataset(verbose=False)
         test_decision_tree = DecisionTree()
         test_decision_tree.set_training_samples_root(test_dataset.samples)
-        test_decision_tree.set_attributes_list(list(range(test_dataset.num_features)))
-        self.assertEqual(test_decision_tree.get_all_possible_values_on_attribute(0),[0,1,2])
-        self.assertEqual(test_decision_tree.get_all_possible_values_on_attribute(1),[0,1,2])
-        self.assertEqual(test_decision_tree.get_all_possible_values_on_attribute(5),[0,1])
+        test_decision_tree.set_attributes_list([(0,0),(1,0),(2,0),(3,0),(4,0),(5,0)])
+        self.assertEqual(test_decision_tree.get_all_possible_values_on_attribute((0,0)),[0,1,2])
+        self.assertEqual(test_decision_tree.get_all_possible_values_on_attribute((1,0)),[0,1,2])
+        self.assertEqual(test_decision_tree.get_all_possible_values_on_attribute((5,0)),[0,1])
 
-    @unittest.skip("skip test_generate_decision_tree")
+    # @unittest.skip("skip test_generate_decision_tree")
     def test_generate_decision_tree(self):
         test_dataset = Dataset(_dataset_name = 'watermelon_2.0', _dataset_file_path = './datasets/watermelon.csv')
         test_dataset.load_dataset(verbose=False)
         test_decision_tree = DecisionTree()
         test_decision_tree.set_training_samples_root(test_dataset.samples)
-        test_decision_tree.set_attributes_list(list(range(test_dataset.num_features)))
+        test_decision_tree.set_attributes_list([(0,0),(1,0),(2,0),(3,0),(4,0),(5,0)])
         decision_tree_root = test_decision_tree.generate_decision_tree(test_decision_tree.training_samples_root,test_decision_tree.attributes_list)
         self.assertEqual(len(decision_tree_root.child_node_list[0].child_node_list[1].child_node_list),3)
         
-    @unittest.skip("skip test_generate_decision_tree_continuous")
+    # @unittest.skip("skip test_generate_decision_tree_continuous")
     def test_generate_decision_tree_continuous(self):
         test_dataset = Dataset(_dataset_name = 'watermelon_3.0', _dataset_file_path = './datasets/watermelon2.csv')
-        test_dataset.load_dataset(verbose=True)
+        test_dataset.load_dataset(verbose=False)
         test_decision_tree = DecisionTree()
         test_decision_tree.set_training_samples_root(test_dataset.samples)
         test_decision_tree.set_attributes_list(list(range(test_dataset.num_features)))
--- test/test_utils.py
+++ test/test_utils.py
@@ -4,19 +4,19 @@ from random_forests.decision_tree import DecisionTree
 from random_forests.utils import VisTree,Dataset
 
 class TestVisTree(unittest.TestCase):
-    @unittest.skip("skip test_vis_tree")
-    def test_vis_tree(self):
+    # @unittest.skip("skip test_vis_tree")
+    def test_vis_tree_1(self):
         test_dataset = Dataset(_dataset_name = 'watermelon_2.0', _dataset_file_path = './datasets/watermelon.csv')
         test_dataset.load_dataset(verbose=False)
         test_decision_tree = DecisionTree()
         test_decision_tree.set_training_samples_root(test_dataset.samples)
-        test_decision_tree.set_attributes_list(list(range(test_dataset.num_features)))
+        test_decision_tree.set_attributes_list([(0,0),(1,0),(2,0),(3,0),(4,0),(5,0)])
         decision_tree_root = test_decision_tree.generate_decision_tree(test_decision_tree.training_samples_root,test_decision_tree.attributes_list)
         test_decision_tree.set_root(decision_tree_root)
         test_vis_tree = VisTree(test_decision_tree,test_dataset.feature2number_mapping,\
             test_dataset.feature_name_list,_tree_name="test_decision_tree")
         test_vis_tree.vis_tree(mode=1)
-        self.assertEqual(1,2-1)
+
 
     def test_vis_tree_2(self):
         test_dataset = Dataset(_dataset_name = 'watermelon_3.0', _dataset_file_path = './datasets/watermelon2.csv')
@@ -30,7 +30,6 @@ class TestVisTree(unittest.TestCase):
         test_vis_tree = VisTree(test_decision_tree,test_dataset.feature2number_mapping,\
             test_dataset.feature_name_list,_tree_name="test_decision_tree_2")
         test_vis_tree.vis_tree(mode=1)
-        self.assertEqual(1,2-1)
 
     def test_vis_tree_3(self):
         test_dataset = Dataset(_dataset_name = 'tennis', _dataset_file_path = './datasets/tennis.csv')
